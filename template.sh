9cabe9372899331e84348574b5bd14ffe79451be
zaid <zaidhsain2@gmail.com>
2026-01-21 21:59:26 +0100

    folder added

---
+++ template.sh
@@ -0,0 +1,11 @@
+ mkdir -p src
+ mkdir -p research
+ touch src/__init__.py
+ touch src/helpers.py
+ touch src/prompt.py
+touch  .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+echo "directory created"
